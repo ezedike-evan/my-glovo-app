74a217a95392d913e5f90ee61ca27c3f2c73a3e6
Ezedike-Evan <egwomevan323@gmail.com>
2026-05-07 21:46:49 +0100

    refactor: update asset paths to absolute URLs in mock data

--- src/lib/mock.js
+++ src/lib/mock.js
@@ -4,50 +4,50 @@ export const topResturants = [
         resuturantname: "the Place",
     },
     {
-        resurantImg: "src/assets/glovoapp.com_b69ae758-72c1-4e02-9794-c979e2af5cbf/egmjz07ibkbr9uopccsc.jpeg",
+        resurantImg: "/assets/glovoapp.com_b69ae758-72c1-4e02-9794-c979e2af5cbf/egmjz07ibkbr9uopccsc.jpeg",
         resuturantname: "Mama cass Resturant",
     },
 
     {
-        resurantImg: "src/assets/glovoapp.com_b69ae758-72c1-4e02-9794-c979e2af5cbf/jui8atcdisz1fyj8xqif.jpeg",
+        resurantImg: "/assets/glovoapp.com_b69ae758-72c1-4e02-9794-c979e2af5cbf/jui8atcdisz1fyj8xqif.jpeg",
         resuturantname: "Sweet Sensation",
     },
     {
-        resurantImg: "src/assets/glovoapp.com_b69ae758-72c1-4e02-9794-c979e2af5cbf/e0pcegpluhtq0akciakd.jpeg",
+        resurantImg: "/assets/glovoapp.com_b69ae758-72c1-4e02-9794-c979e2af5cbf/e0pcegpluhtq0akciakd.jpeg",
         resuturantname: "Chopnownow",
     },
     {
-        resurantImg: "src/assets/glovoapp.com_b69ae758-72c1-4e02-9794-c979e2af5cbf/o57akgxyabkgo04kjlbu.jpeg",
+        resurantImg: "/assets/glovoapp.com_b69ae758-72c1-4e02-9794-c979e2af5cbf/o57akgxyabkgo04kjlbu.jpeg",
         resuturantname: "KFC",
     },
     {
-        resurantImg: "src/assets/glovoapp.com_b69ae758-72c1-4e02-9794-c979e2af5cbf/zdpfnwewlbzc40surjgw.jpeg",
+        resurantImg: "/assets/glovoapp.com_b69ae758-72c1-4e02-9794-c979e2af5cbf/zdpfnwewlbzc40surjgw.jpeg",
         resuturantname: "Fresh Kitchen Resturant",
     },
     {
-        resurantImg: "src/assets/glovoapp.com_b69ae758-72c1-4e02-9794-c979e2af5cbf/nem66tghxmqigr2fodaq.jpeg",
+        resurantImg: "/assets/glovoapp.com_b69ae758-72c1-4e02-9794-c979e2af5cbf/nem66tghxmqigr2fodaq.jpeg",
         resuturantname: "small Chops.ng",
     },
     {
-        resurantImg: "src/assets/glovoapp.com_b69ae758-72c1-4e02-9794-c979e2af5cbf/cg4sdskoaecoxhp7lhkt.jpeg",
+        resurantImg: "/assets/glovoapp.com_b69ae758-72c1-4e02-9794-c979e2af5cbf/cg4sdskoaecoxhp7lhkt.jpeg",
         resuturantname: "Debonairs pizza",
     }
 ]
  export  const delivery =[
     {
-        deliveryImg:"src/assets/glovoapp.com_b69ae758-72c1-4e02-9794-c979e2af5cbf/restaurants.e082f46b.svg",
+        deliveryImg:"/assets/glovoapp.com_b69ae758-72c1-4e02-9794-c979e2af5cbf/restaurants.e082f46b.svg",
         deliveryTitle:"Your city's top resturants",
         deliveryName:"with great variety of resturant you can order your favourite food or ",
         deliveryTag:" explore new resturant nearby"
     },
     {
-        deliveryImg:"src/assets/glovoapp.com_b69ae758-72c1-4e02-9794-c979e2af5cbf/delivery.81e9087f.svg",
+        deliveryImg:"/assets/glovoapp.com_b69ae758-72c1-4e02-9794-c979e2af5cbf/delivery.81e9087f.svg",
         deliveryTitle:"Fast delivery",
         deliveryName:"Like a flash Order or send anything in your city and ",
         deliveryTag:" recieve in minutes"
     },
     {
-        deliveryImg:"src/assets/glovoapp.com_b69ae758-72c1-4e02-9794-c979e2af5cbf/groceries.3dd22a11.svg",
+        deliveryImg:"/assets/glovoapp.com_b69ae758-72c1-4e02-9794-c979e2af5cbf/groceries.3dd22a11.svg",
         deliveryTitle:"Groceries delivery & more",
         deliveryName:"find anything you need from ",
         deliveryTag:" supermarkets to shops pharmacies to florists ",
@@ -57,21 +57,21 @@ export const topResturants = [
 
  export const body =[
     {
-        bodyImg:"src/assets/glovoapp.com_b69ae758-72c1-4e02-9794-c979e2af5cbf/rider-blob-new.aa1e6260.png",
+        bodyImg:"/assets/glovoapp.com_b69ae758-72c1-4e02-9794-c979e2af5cbf/rider-blob-new.aa1e6260.png",
         bodyTitle:"Become a rider",
         bodyText:"Enjoy flexibility freedom and competitive learning by delivering through glovo",
         bodyButton:"Register here"
     },
 
     {
-        bodyImg:"src/assets/glovoapp.com_b69ae758-72c1-4e02-9794-c979e2af5cbf/partners-blob-new.1aae464a.png",
+        bodyImg:"/assets/glovoapp.com_b69ae758-72c1-4e02-9794-c979e2af5cbf/partners-blob-new.1aae464a.png",
         bodyTitle:"Become a partner",
         bodyText:"Grow with Glovo! Our technology and user base can help you build sales and unlock new opportunities",
         bodyButton:"Register here"
     },
 
     {
-        bodyImg:"src/assets/glovoapp.com_b69ae758-72c1-4e02-9794-c979e2af5cbf/careers-blob-new.b9e63af8.png",
+        bodyImg:"/assets/glovoapp.com_b69ae758-72c1-4e02-9794-c979e2af5cbf/careers-blob-new.b9e63af8.png",
         bodyTitle:"Careers",
         bodyText:"Ready for an exciting new challange? if you're ambitious humble and love working with others, then we want to hear from you ",
         bodyButton:"Register here"
@@ -86,38 +86,38 @@ export const topResturants = [
         category:"breakfast"
     },
     {
-        resurantImg: "src/assets/glovoapp.com_b69ae758-72c1-4e02-9794-c979e2af5cbf/egmjz07ibkbr9uopccsc.jpeg",
+        resurantImg: "/assets/glovoapp.com_b69ae758-72c1-4e02-9794-c979e2af5cbf/egmjz07ibkbr9uopccsc.jpeg",
         resuturantname: "Mama cass Resturant",
         category:"burger"
     },
     
     {
-        resurantImg: "src/assets/glovoapp.com_b69ae758-72c1-4e02-9794-c979e2af5cbf/jui8atcdisz1fyj8xqif.jpeg",
+        resurantImg: "/assets/glovoapp.com_b69ae758-72c1-4e02-9794-c979e2af5cbf/jui8atcdisz1fyj8xqif.jpeg",
         resuturantname: "Sweet Sensation",
         category:"healthy"
     },
     {
-        resurantImg: "src/assets/glovoapp.com_b69ae758-72c1-4e02-9794-c979e2af5cbf/e0pcegpluhtq0akciakd.jpeg",
+        resurantImg: "/assets/glovoapp.com_b69ae758-72c1-4e02-9794-c979e2af5cbf/e0pcegpluhtq0akciakd.jpeg",
         resuturantname: "Chopnownow",
         category:"india"
     },
     {
-        resurantImg: "src/assets/glovoapp.com_b69ae758-72c1-4e02-9794-c979e2af5cbf/o57akgxyabkgo04kjlbu.jpeg",
+        resurantImg: "/assets/glovoapp.com_b69ae758-72c1-4e02-9794-c979e2af5cbf/o57akgxyabkgo04kjlbu.jpeg",
         resuturantname: "KFC",
         category:"pastery"
     },
     {
-        resurantImg: "src/assets/glovoapp.com_b69ae758-72c1-4e02-9794-c979e2af5cbf/zdpfnwewlbzc40surjgw.jpeg",
+        resurantImg: "/assets/glovoapp.com_b69ae758-72c1-4e02-9794-c979e2af5cbf/zdpfnwewlbzc40surjgw.jpeg",
         resuturantname: "Fresh Kitchen Resturant",
         category:"sushi"
     },
     {
-        resurantImg: "src/assets/glovoapp.com_b69ae758-72c1-4e02-9794-c979e2af5cbf/cg4sdskoaecoxhp7lhkt.jpeg",
+        resurantImg: "/assets/glovoapp.com_b69ae758-72c1-4e02-9794-c979e2af5cbf/cg4sdskoaecoxhp7lhkt.jpeg",
         resuturantname: "Debonairs pizza",
         category:"pizza"
     },
     {
-        resurantImg: "src/assets/glovoapp.com_b69ae758-72c1-4e02-9794-c979e2af5cbf/nem66tghxmqigr2fodaq.jpeg",
+        resurantImg: "/assets/glovoapp.com_b69ae758-72c1-4e02-9794-c979e2af5cbf/nem66tghxmqigr2fodaq.jpeg",
         resuturantname: "small Chops.ng",
         category:"Nigerian"
     },
