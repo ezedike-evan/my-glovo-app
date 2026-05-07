d52df9b9ce94b1e499e4d6ed405002d92c12a2bf
Maryjane Nmesoma <nmesomman@gmail.com>
2026-05-07 17:00:17 +0100

    style: completed all tablet reponsive design

--- src/Component/Section.jsx
+++ src/Component/Section.jsx
@@ -5,16 +5,16 @@ import "../Component/CSS/Section.css"
 const Section = () => {
   return (
     <div className='downloadSection'>
-       <div>
-         <img src= "src/assets/glovoapp.com_b69ae758-72c1-4e02-9794-c979e2af5cbf/download-icon.475cc763.svg" alt=""  className='img-download'/>
-         <p className='text1'>Download the app</p>
-         <p className='text2'>order anyrhing and track it in real time with glovo app </p>
-        <img src="src/assets/glovoapp.com_b69ae758-72c1-4e02-9794-c979e2af5cbf/ios-button.33217f86.svg" alt="" />
-        <img src="src/assets/glovoapp.com_b69ae758-72c1-4e02-9794-c979e2af5cbf/google-button.4b60c502.svg" alt="" />
-       </div>
-       <div>
-        <img src="src/assets/glovoapp.com_b69ae758-72c1-4e02-9794-c979e2af5cbf/download-app-new.2d9bc196.png" alt=""  className='phoneImg'/> 
-       </div>
+      <div className='downloadHolder'>
+        <img src= "src/assets/glovoapp.com_b69ae758-72c1-4e02-9794-c979e2af5cbf/download-icon.475cc763.svg" alt=""  className='img-download'/>
+        <p className='text1'>Download the app</p>
+        <p className='text2'>order anyrhing and track it in real time with glovo app </p>
+        <div>
+          <img src="src/assets/glovoapp.com_b69ae758-72c1-4e02-9794-c979e2af5cbf/ios-button.33217f86.svg" alt="" />
+          <img src="src/assets/glovoapp.com_b69ae758-72c1-4e02-9794-c979e2af5cbf/google-button.4b60c502.svg" alt="" />
+        </div>
+      </div>
+      <img src="src/assets/glovoapp.com_b69ae758-72c1-4e02-9794-c979e2af5cbf/download-app-new.2d9bc196.png" alt=""  className='phoneImg'/>
     </div>
   )
 }
